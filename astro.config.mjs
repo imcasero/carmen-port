6786b102ea5f3eac1ae770d60d2495fda2a6cdf5
Imcasero <diegocaserosmr@gmail.com>
2024-01-19 17:21:52 +0100

    fix: new form whit html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  renderers: ["@astrojs/renderer-react"] // Agrega esta línea para habilitar el renderizador de React
+  integrations: [tailwind()],
 });
